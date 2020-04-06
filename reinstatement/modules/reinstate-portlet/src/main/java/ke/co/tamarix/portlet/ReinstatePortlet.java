cc1e768edecd496ef268913877e9a12c20a797fa
ochieng <developer.humphrey@gmail.com>
2020-04-06 13:04:23 +0300

    Done with FA records

--- reinstatement/modules/reinstate-portlet/src/main/java/ke/co/tamarix/portlet/ReinstatePortlet.java
+++ reinstatement/modules/reinstate-portlet/src/main/java/ke/co/tamarix/portlet/ReinstatePortlet.java
@@ -2,11 +2,31 @@ package ke.co.tamarix.portlet;
 
 import ke.co.tamarix.constants.ReinstatePortletKeys;
 
+import com.liferay.counter.kernel.service.CounterLocalService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactory;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.util.ParamUtil;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.ProcessAction;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import ke.co.tamarix.service.ReinstatementLocalService;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import ke.co.tamarix.model.Reinstatement;
 
 /**
  * @author ochieng
@@ -27,4 +47,46 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class ReinstatePortlet extends MVCPortlet {
+	
+	private Log log = LogFactoryUtil.getLog(this.getClass().getName());
+	
+	@Reference
+    CounterLocalService counterLocalService;
+	
+	@Reference
+	ReinstatementLocalService reinstatementLocalService;
+	
+	@Override
+		public void render(RenderRequest renderRequest, RenderResponse renderResponse)
+				throws IOException, PortletException {
+			// TODO Auto-generated method stub
+		List<Reinstatement> reinstatementList = reinstatementLocalService.getReinstatements(QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+        renderRequest.setAttribute("reinstatementList", reinstatementList);
+			super.render(renderRequest, renderResponse);
+		}
+	
+	@ProcessAction(name = "addReinstatement")
+    public void addReinstatement(ActionRequest actionRequest,ActionResponse actionResponse) {
+        long reinstatementId = counterLocalService.increment(Reinstatement.class.getName());
+        String firstName = ParamUtil.getString(actionRequest, "firstName");
+        String lastName = ParamUtil.getString(actionRequest, "lastName");
+        String branchName = ParamUtil.getString(actionRequest, "branchName");
+        String payrollNo = ParamUtil.getString(actionRequest, "payrollNo");
+        String terminationReason = ParamUtil.getString(actionRequest, "terminationReason");
+        String terminationCode = ParamUtil.getString(actionRequest, "terminationCode");
+        String formerBranch = ParamUtil.getString(actionRequest, "formerBranch");
+        
+        Reinstatement reinstatement = reinstatementLocalService.createReinstatement(reinstatementId);
+        reinstatement.setReinstatementId(reinstatementId);
+        reinstatement.setFirstName(firstName);
+        reinstatement.setLastName(lastName);
+        reinstatement.setBranchName(branchName);
+        reinstatement.setPayrollNo(payrollNo);        
+        reinstatement.setTerminationReason(terminationReason);
+        reinstatement.setTerminationCode(terminationCode);
+        reinstatement.setFormerBranch(formerBranch);
+        
+        reinstatementLocalService.addReinstatement(reinstatement);
+    }
+	
 }
